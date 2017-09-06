f302e2cf0887d318cf2a1395d7e1acb216b6c972
Post, Stefan <stefan.post@aspect.com>
2017-09-06 12:18:42 +0200

    add

--- src/main/java/Extractor.java
+++ src/main/java/Extractor.java
@@ -1,3 +1,4 @@
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -10,65 +11,53 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Extractor
-{
+public class Extractor {
 
-    public static void main( final String[] args )
-    {
-        final List<Player> tor = parseHtml( "tor" );
-        final List<Player> abwehr = parseHtml( "abwehr" );
-        final List<Player> mittelfeld = parseHtml( "mittelfeld" );
-        final List<Player> sturm = parseHtml( "sturm" );
+    public static void main(final String[] args) {
+        final List<Player> tor = parseHtml("tor");
+        final List<Player> abwehr = parseHtml("abwehr");
+        final List<Player> mittelfeld = parseHtml("mittelfeld");
+        final List<Player> sturm = parseHtml("sturm");
 
-        TeamCreator.createTeam( tor, abwehr, mittelfeld, sturm, 1, 3, 4, 3 );
+        TeamCreator.createTeamStefan(tor, abwehr, mittelfeld, sturm);
+        TeamCreator.createTeam(tor, abwehr, mittelfeld, sturm, 1, 3, 4, 3);
     }
 
-    private static List<Player> parseHtml( final String filename )
-    {
+    private static List<Player> parseHtml(final String filename) {
         Document doc = null;
-        try
-        {
-            doc = Jsoup.parse( readFile( "html/" + filename + ".htm", Charset.defaultCharset() ) );
-        }
-        catch ( final IOException e )
-        {
+        try {
+            doc = Jsoup.parse(readFile("html/" + filename + ".htm", Charset.defaultCharset()));
+        } catch (final IOException e) {
             e.printStackTrace();
         }
-        final Element playerTable = doc.select( "#ovContent > div.tborder-b-s > div > div > div > table > tbody" ).first();
+        final Element playerTable = doc.select("#ovContent > div.tborder-b-s > div > div > div > table > tbody").first();
         final List<Player> playerList = new ArrayList<>();
-        for ( final Element child : playerTable.children() )
-        {
-            if ( "alt".equals( child.className() )
-                    || ( !child.outerHtml().contains( "headlink" ) && child.className().isEmpty() ) )
-            {
-                playerList.add( createPlayer( child ) );
+        for (final Element child : playerTable.children()) {
+            if ("alt".equals(child.className())
+                    || (!child.outerHtml().contains("headlink") && child.className().isEmpty())) {
+                playerList.add(createPlayer(child));
             }
         }
         return playerList;
     }
 
-    private static Player createPlayer( final Element child )
-    {
-        try
-        {
-            final String name = ( ( TextNode ) child.childNodes().get( 3 ).childNode( 0 ).childNode( 0 ) ).text();
-            final String pos = ( ( TextNode ) child.childNodes().get( 7 ).childNode( 0 ) ).text();
-            final int pointsThis = ( child.childNodes().get( 11 ).childNodeSize() > 0 ) ? Integer.valueOf( ( ( TextNode ) child.childNodes().get( 11 ).childNode( 0 ) ).text() ) : 0;
-            final float price = Float.valueOf( ( ( TextNode ) child.childNodes().get( 9 ).childNode( 0 ) ).text().replace( ',', '.' ) );
-            final int pointsLast = ( child.childNodes().get( 15 ).childNodeSize() > 0 ) ? Integer.valueOf( ( ( TextNode ) child.childNodes().get( 15 ).childNode( 0 ) ).text() ) : 0;
-            return new Player( name, pos, pointsThis, price, pointsLast );
-        }
-        catch ( final Exception e )
-        {
+    private static Player createPlayer(final Element child) {
+        try {
+            final String name = ((TextNode) child.childNodes().get(3).childNode(0).childNode(0)).text();
+            final String pos = ((TextNode) child.childNodes().get(7).childNode(0)).text();
+            final int pointsThis = (child.childNodes().get(11).childNodeSize() > 0) ? Integer.valueOf(((TextNode) child.childNodes().get(11).childNode(0)).text()) : 0;
+            final float price = Float.valueOf(((TextNode) child.childNodes().get(9).childNode(0)).text().replace(',', '.'));
+            final int pointsLast = (child.childNodes().get(15).childNodeSize() > 0) ? Integer.valueOf(((TextNode) child.childNodes().get(15).childNode(0)).text()) : 0;
+            return new Player(name, pos, pointsThis, price, pointsLast);
+        } catch (final Exception e) {
             final int a = 5;
         }
         return null;
     }
 
-    private static String readFile( final String path, final Charset encoding )
-            throws IOException
-    {
-        final byte[] encoded = Files.readAllBytes( Paths.get( path ) );
-        return new String( encoded, encoding );
+    private static String readFile(final String path, final Charset encoding)
+            throws IOException {
+        final byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
     }
 }
--- src/main/java/TeamCreator.java
+++ src/main/java/TeamCreator.java
@@ -1,64 +1,48 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
-public class TeamCreator
-{
+public class TeamCreator {
 
     private static final float BUDGET = 42.5f;
 
-    static void createTeam( final List<Player> tor, final List<Player> abwehr, final List<Player> mittelfeld, final List<Player> sturm, final int cTor, final int cAbw, final int cMf, final int cSt )
-    {
-        final float maxBudget = ( float ) ( BUDGET - ( 0.2 * ( 22 - ( cAbw + cMf + cSt + cTor ) ) ) );
+    static void createTeam(final List<Player> tor, final List<Player> abwehr, final List<Player> mittelfeld, final List<Player> sturm, final int cTor, final int cAbw, final int cMf, final int cSt) {
+        final float maxBudget = (float) (BUDGET - (0.2 * (22 - (cAbw + cMf + cSt + cTor))));
         int maxPts = 0;
         List<Player> bestTeam = new ArrayList<>();
-        for ( int i = 0; i < 1000000; i++ )
-        {
+        for (int i = 0; i < 1000000; i++) {
             final List<Player> allPlayers = new ArrayList<>();
-            allPlayers.addAll( tor );
-            allPlayers.addAll( abwehr );
-            allPlayers.addAll( mittelfeld );
-            allPlayers.addAll( sturm );
-            allPlayers.sort( new Comparator<Player>()
-            {
+            allPlayers.addAll(tor);
+            allPlayers.addAll(abwehr);
+            allPlayers.addAll(mittelfeld);
+            allPlayers.addAll(sturm);
+            allPlayers.sort(new Comparator<Player>() {
                 @Override
-                public int compare( final Player o1, final Player o2 )
-                {
-                    return ( int ) ( o2._ptsPredictionPerMio - o1._ptsPredictionPerMio );
+                public int compare(final Player o1, final Player o2) {
+                    return (int) (o2._ptsPredictionPerMio - o1._ptsPredictionPerMio);
                 }
-            } );
+            });
             final HashMap<Player.POS, Integer> posiCount = new HashMap<>();
-            posiCount.put( Player.POS.TOR, cTor );
-            posiCount.put( Player.POS.ABW, cAbw );
-            posiCount.put( Player.POS.MIT, cMf );
-            posiCount.put( Player.POS.STU, cSt );
+            posiCount.put(Player.POS.TOR, cTor);
+            posiCount.put(Player.POS.ABW, cAbw);
+            posiCount.put(Player.POS.MIT, cMf);
+            posiCount.put(Player.POS.STU, cSt);
             double budget = 0;
             boolean teamIsFull = false;
             final List<Player> team = new ArrayList<>();
-            do
-            {
-                for ( final Player player : allPlayers )
-                {
-                    if ( !team.contains( player ) )
-                    {
-                        if ( posiCount.get( player._pos ) > 0 && budget + player._price < maxBudget )
-                        {
-                            team.add( player );
-                            posiCount.put( player._pos, posiCount.get( player._pos ) - 1 );
+            do {
+                for (final Player player : allPlayers) {
+                    if (!team.contains(player)) {
+                        if (posiCount.get(player._pos) > 0 && budget + player._price < maxBudget) {
+                            team.add(player);
+                            posiCount.put(player._pos, posiCount.get(player._pos) - 1);
                             budget += player._price;
-                        }
-                        else if ( posiCount.get( player._pos ) > 0 )
-                        {
-                            final Player toRemove = team.remove( ( int ) ( ( Math.random() * team.size() ) ) );
+                        } else if (posiCount.get(player._pos) > 0) {
+                            final Player toRemove = team.remove((int) ((Math.random() * team.size())));
                             budget -= toRemove._price;
-                            posiCount.put( toRemove._pos, posiCount.get( toRemove._pos ) + 1 );
+                            posiCount.put(toRemove._pos, posiCount.get(toRemove._pos) + 1);
                         }
                         teamIsFull = true;
-                        for ( final Integer count : posiCount.values() )
-                        {
-                            if ( count > 0 )
-                            {
+                        for (final Integer count : posiCount.values()) {
+                            if (count > 0) {
                                 teamIsFull = false;
                                 break;
                             }
@@ -66,26 +50,107 @@ public class TeamCreator
                     }
                 }
             }
-            while ( !teamIsFull );
+            while (!teamIsFull);
             int ptsLastSeason = 0;
-            for ( final Player player : team )
-            {
+            for (final Player player : team) {
                 ptsLastSeason += player._ptsPrediction;
             }
-            if ( maxPts < ptsLastSeason )
-            {
+            if (maxPts < ptsLastSeason) {
                 maxPts = ptsLastSeason;
                 bestTeam = team;
             }
         }
-        System.out.println( "Whole team: " + maxPts );
+        System.out.println("Whole team: " + maxPts);
 
         float gesamt = 0;
-        for ( final Player player : bestTeam )
-        {
-            System.out.println( player._name + " " + player._ptsPrediction );
+        for (final Player player : bestTeam) {
+            System.out.println(player._name + " " + player._ptsPrediction);
             gesamt += player._price;
         }
-        System.out.println( "Costs: " + gesamt );
+        System.out.println("Costs: " + gesamt);
+    }
+
+    private static void refreshMaxima(final EnumMap<Player.POS, Integer> posiCount, final EnumMap<Player.POS, Integer> posiMaxCount) {
+        if (posiCount.get(Player.POS.STU) == 3) {
+            posiMaxCount.replace(Player.POS.MIT, 4);
+        }
+        if (posiCount.get(Player.POS.MIT) == 5 && posiCount.get(Player.POS.STU) == 2) {
+            posiMaxCount.replace(Player.POS.ABW, 3);
+            posiMaxCount.replace(Player.POS.STU, 2);
+        } else if (posiCount.get(Player.POS.MIT) == 5 && posiCount.get(Player.POS.ABW) == 4) {
+            posiMaxCount.replace(Player.POS.STU, 1);
+        } else if (posiCount.get(Player.POS.MIT) == 5) {
+            posiMaxCount.replace(Player.POS.STU, 2);
+        }
+    }
+
+    static void createTeamStefan(final List<Player> tor, final List<Player> abwehr, final List<Player> mittelfeld, final List<Player> sturm) {
+        double currentBudget = (float) (BUDGET - (0.2 * 22));
+
+        int maxPts = 0;
+        List<Player> bestTeam = new ArrayList<>();
+//        for (int i = 0; i < 1000000; i++) {
+        final List<Player> allPlayers = new ArrayList<>();
+        allPlayers.addAll(tor);
+        allPlayers.addAll(abwehr);
+        allPlayers.addAll(mittelfeld);
+        allPlayers.addAll(sturm);
+        allPlayers.sort(new Comparator<Player>() {
+            @Override
+            public int compare(final Player o1, final Player o2) {
+                return (int) (o2._ptsPredictionPerMio - o1._ptsPredictionPerMio);
+            }
+        });
+        final EnumMap<Player.POS, Integer> posiCount = new EnumMap<>(Player.POS.class);
+        posiCount.put(Player.POS.TOR, 0);
+        posiCount.put(Player.POS.ABW, 0);
+        posiCount.put(Player.POS.MIT, 0);
+        posiCount.put(Player.POS.STU, 0);
+        final EnumMap<Player.POS, Integer> posiMaxCount = new EnumMap<>(Player.POS.class);
+        posiMaxCount.put(Player.POS.TOR, 1);
+        posiMaxCount.put(Player.POS.ABW, 4);
+        posiMaxCount.put(Player.POS.MIT, 5);
+        posiMaxCount.put(Player.POS.STU, 3);
+        double budget = 0;
+        boolean teamIsFull = false;
+        final List<Player> team = new ArrayList<>();
+        do {
+            for (final Player player : allPlayers) {
+                if (!team.contains(player)) {
+                    if (posiCount.get(player._pos) <= posiMaxCount.get(player._pos) && budget + player._price < currentBudget) {
+                        team.add(player);
+                        posiCount.put(player._pos, posiCount.get(player._pos) + 1);
+                        budget += player._price;
+                        currentBudget = currentBudget - budget - 0.2;
+                        refreshMaxima(posiCount, posiMaxCount);
+                    }
+                    teamIsFull = true;
+                    for (final Integer count : posiCount.values()) {
+                        if (count > 0) {
+                            teamIsFull = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        while (!teamIsFull);
+        int ptsLastSeason = 0;
+        for (final Player player : team) {
+            ptsLastSeason += player._ptsPrediction;
+        }
+        if (maxPts < ptsLastSeason) {
+            maxPts = ptsLastSeason;
+            bestTeam = team;
+        }
+//        }
+        System.out.println("Whole team: " + maxPts);
+
+        float gesamt = 0;
+        for (final Player player : bestTeam) {
+            System.out.println(player._name + " " + player._ptsPrediction);
+            gesamt += player._price;
+        }
+        System.out.println("Costs: " + gesamt);
     }
 }
